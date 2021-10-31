e54fdefc07468324a118494cd246f852e0a4b048
JavaCoder2021 <92649923+JavaCoder2021@users.noreply.github.com>
2021-10-31 20:38:08 +0200

    Create Task4.java

---
+++ Working with a string as an array of characters/Task4.java
@@ -0,0 +1,25 @@
+//4. В строке найти количество чисел.
+
+package task4;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        
+        String str = "I have 22 bananas, 121 pencils, and 32 apples";
+        System.out.println(str);
+        
+        String[] sarr = str.split(" ");
+        int count = 0;
+        for(String str1 : sarr) {
+            try {
+                Integer.parseInt(str1);
+            } catch(NumberFormatException e) {
+                count++;
+            }
+        }     
+        System.out.println("Amount of numbers: " + (sarr.length - count));
+            
+    }
+    
+}
