1e7b151e03ab10bc6127a3ac0e5460fc70eb418c
JavaCoder2021 <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-01 19:25:14 +0200

    Create Task2.java

---
+++ Working with a string as an object of type String or StringBuilder/Task2.java
@@ -0,0 +1,35 @@
+//2. В строке вставить после каждого символа 'a' символ 'b'.  
+
+package task2;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        String s = "I have 22 bananas, 121 pencils, and 32 apples";
+        System.out.println(s);
+        
+        String newStr = replaceString(s);
+        
+        System.out.println("New string = " + newStr);
+        
+    }
+    
+    public static String replaceString(String s) {
+        
+        char[] str = s.toCharArray();
+        String newStr = "";
+        
+        for (int i = 0; i < str.length; i++)
+        {
+            if (str[i] == 'a') 
+                newStr += "ab";
+            else 
+                newStr += str[i];   
+        }
+
+        return newStr;
+        
+    }
+    
+}
