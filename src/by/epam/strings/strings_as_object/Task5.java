53c524e43b99398231bb3b23d40d870139e56f43
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-02 21:30:45 +0200

    Create Task5.java

---
+++ src/by/epam/strings/strings_as_object/Task5.java
@@ -0,0 +1,49 @@
+/*
+5. Подсчитать, сколько раз среди символов заданной строки встречается буква “а”. 
+*/
+
+package by.epam.strings.strings_as_object;
+
+import java.util.Scanner;
+
+public class Task5 {
+
+    public static void main(String[] args) {
+
+        String str = input();
+        
+        int count = characters(str);
+        
+        output(count);
+        
+    }
+    
+    private static String input() {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Line = ");
+        String str = input.nextLine();
+        
+        return str;
+        
+    }    
+    
+    public static int characters(String str) {
+
+        int count = 0;
+        
+        for (char c: str.toCharArray()) 
+            if (c == 'а' || c == 'a') //Russin and English 'a'
+                count++;
+                        
+        return count;
+       
+   }
+    
+    private static void output(int count) {
+        
+        System.out.println("count = " + count);
+        
+    }
+    
+}
