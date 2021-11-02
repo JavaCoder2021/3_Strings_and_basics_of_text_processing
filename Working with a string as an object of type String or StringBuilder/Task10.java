b0254a31f9b83f356b79762b5f163da0a22e8b3b
JavaCoder2021 <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-02 19:20:17 +0200

    Create Task10.java

---
+++ Working with a string as an object of type String or StringBuilder/Task10.java
@@ -0,0 +1,48 @@
+//10. Строка X состоит из нескольких предложений, каждое из которых кончается точкой, восклицательным 
+//или вопросительным знаком. Определить количество предложений в строке X. 
+
+package task10;
+
+import java.util.Scanner;
+
+public class Task10 {
+
+    public static void main(String[] args) {
+
+        //Input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Line = ");
+        String str = input.nextLine();
+        
+        int numberSentences = countOfSentences(str);
+        
+        System.out.println("Number of offers = " + numberSentences);
+        
+    }
+    
+     public static int countOfSentences(String str) {
+
+        int numberSentences = 0;
+        
+        for ( char tmp: str.toCharArray() )
+        {
+            if (isEndOfnumberSentence(tmp))   
+                numberSentences++;            
+        }
+        
+        return numberSentences;
+         
+    }
+    
+    public static boolean isEndOfnumberSentence(char tmp) {
+        
+        boolean isEnd = false;
+
+        if (tmp == '.' || tmp == '!' || tmp == '?') 
+            isEnd = true;
+
+        return isEnd;
+        
+    }
+    
+}
