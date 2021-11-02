38be5ea1fbef7d27de3ddf08e518aa76639cbffb
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-02 19:54:46 +0200

    Create Task10.java

---
+++ src/by/epam/strings/strings_as_object/Task10.java
@@ -0,0 +1,54 @@
+/*
+10. Строка X состоит из нескольких предложений, каждое из которых кончается точкой, 
+восклицательным или вопросительным знаком. Определить количество предложений в строке X.
+*/
+
+package by.epam.strings.strings_as_object;
+
+import java.util.Scanner;
+
+public class Task10 {
+
+    public static void main(String[] args) {
+
+        //Input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Line = ");
+        String str = input.nextLine();
+                
+        output(countOfSentences(str));
+        
+    }
+    
+    private static boolean isEndOfnumberSentence(char tmp) {
+        
+        boolean isEnd = false;
+
+        if (tmp == '.' || tmp == '!' || tmp == '?') 
+            isEnd = true;
+
+        return isEnd;
+        
+    }
+    
+    private static int countOfSentences(String str) {
+
+        int numberSentences = 0;
+        
+        for ( char tmp: str.toCharArray() )
+        {
+            if (isEndOfnumberSentence(tmp))   
+                numberSentences++;            
+        }
+        
+        return numberSentences;
+         
+    }
+    
+    private static void output (int numberSentences) {
+        
+        System.out.println("Number of offers = " + numberSentences);
+        
+    }
+    
+}
