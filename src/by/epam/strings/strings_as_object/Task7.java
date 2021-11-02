9bfeb0e82af28a2f60216eb22addcd2c8edb83c6
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-02 21:16:04 +0200

    Create Task7.java

---
+++ src/by/epam/strings/strings_as_object/Task7.java
@@ -0,0 +1,58 @@
+/*
+7. Вводится строка. Требуется удалить из нее повторяющиеся символы и все пробелы. 
+Например, если было введено "abc cde def", то должно быть выведено "abcdef".
+*/
+
+package by.epam.strings.strings_as_object;
+
+import java.util.Scanner;
+import java.util.LinkedHashSet;
+
+public class Task7 {
+
+    public static void main(String[] args) {
+
+        String str = input();
+        
+        str = removeDuplicateCharacters(str);
+               
+        output(str);
+        
+    }
+    
+    private static String input() {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Line = ");
+        String str = input.nextLine();
+        
+        return str;
+        
+    }
+    
+    private static String removeDuplicateCharacters(String str) {
+        
+        LinkedHashSet<Character> temp = new LinkedHashSet<>();
+        StringBuilder builder = new StringBuilder();
+ 
+        str = str.replace(" ", "");
+ 
+        for (int i = 0; i < str.length(); i++) 
+            if (temp.add(str.charAt(i)))
+            {
+                builder.append(str.charAt(i));
+            }    
+        
+        str = builder.toString();
+        
+        return str;
+        
+    }
+    
+    private static void output(String str) {
+        
+        System.out.println("New line = " + str);
+        
+    }
+
+}
