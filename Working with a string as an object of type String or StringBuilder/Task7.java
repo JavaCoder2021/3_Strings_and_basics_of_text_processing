799ee5d9b60b1b57c0fd44bed9ee293aea13ef28
JavaCoder2021 <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-02 17:55:49 +0200

    Create Task7.java

---
+++ Working with a string as an object of type String or StringBuilder/Task7.java
@@ -0,0 +1,33 @@
+// 7. Вводится строка. Требуется удалить из нее повторяющиеся символы и все пробелы. 
+//Например, если было введено "abc cde def", то должно быть выведено "abcdef". 
+
+package task7;
+
+import java.util.*;
+
+public class Task7 {
+
+    public static void main(String[] args) {
+
+        //Input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Line = ");
+        String str = input.nextLine(); 
+        
+        LinkedHashSet<Character> temp = new LinkedHashSet<>();
+        StringBuilder builder = new StringBuilder();
+ 
+        str = str.replace(" ", "");
+ 
+        for (int i = 0; i < str.length(); i++) 
+            if (temp.add(str.charAt(i)))
+            {
+                builder.append(str.charAt(i));
+            }    
+        
+        String newString = builder.toString(); 
+        System.out.println("New line = " + newString);
+        
+    }
+
+}
