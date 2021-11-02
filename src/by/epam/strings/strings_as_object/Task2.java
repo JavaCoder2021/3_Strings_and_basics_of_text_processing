b03346fdfdc7481d0b97a65b2eb6a444c363fce7
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-02 21:50:25 +0200

    Create Task2.java

---
+++ src/by/epam/strings/strings_as_object/Task2.java
@@ -0,0 +1,43 @@
+/*
+2. В строке вставить после каждого символа 'a' символ 'b'. 
+*/
+
+package by.epam.strings.strings_as_object;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        String s = "I have 22 bananas, 121 pencils, and 32 apples";
+        System.out.println("Line = " + s);
+        
+        String newStr = replaceString(s);
+        
+        output(newStr);
+        
+    }
+    
+    private static String replaceString(String s) {
+        
+        char[] str = s.toCharArray();
+        String newStr = "";
+        
+        for (int i = 0; i < str.length; i++)
+        {
+            if (str[i] == 'a' || str[i] == 'а') //Russian and English 'a'
+                newStr += "ab";
+            else 
+                newStr += str[i];   
+        }
+
+        return newStr;
+        
+    }
+    
+    private static void output(String str) {
+        
+        System.out.println("New line = " + str);
+        
+    }  
+    
+}
