4b8e398ff463aa5e16fc9ea9ca1317a4d8e29bf0
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-06 18:21:56 +0200

    Create Task3.java

---
+++ src/by/epam/strings/strings_as_chars_array/Task3.java
@@ -0,0 +1,33 @@
+/*
+3. В строке найти количество цифр. 
+*/
+
+package by.epam.strings.strings_as_chars_array;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        
+        String str = "I have 22 bananas, 121 pencils, and 32 apples";
+        System.out.println(str);
+        
+        int count = numberOfDigits(str);
+        
+        System.out.println("Number of digits: " + count);
+            
+    }
+    
+    public static int numberOfDigits(String str) {
+        
+        char[] tmp = str.toCharArray();
+        int count = 0;
+        
+        for (int i = 0; i < tmp.length; i++)
+            if (Character.isDigit(tmp[i]))
+                count++;
+        
+        return count;
+        
+    }
+    
+}
