f971dc59dce4dd67180ce2bb657365f3d57abcf0
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-12-11 20:50:43 +0200

    Update Task5.java

--- src/by/epam/strings/strings_as_chars_array/Task5.java
+++ src/by/epam/strings/strings_as_chars_array/Task5.java
@@ -1,7 +1,9 @@
 /*
-5.  Удалить  в  строке  все  лишние  пробелы,  то  есть  серии  подряд  идущих  пробелов  
-заменить  на  одиночные  пробелы. Крайние пробелы в строке удалить. 
-*/
+ * 5.  
+ * Удалить в строке все лишние пробелы, то есть серии подряд идущих пробелов  
+ * заменить на одиночные пробелы. 
+ * Крайние пробелы в строке удалить. 
+ */
 
 package by.epam.strings.strings_as_chars_array;
 
@@ -18,13 +20,13 @@ public class Task5 {
     
     public static String removeSpaces(String str) {
         
-        final String twoSpaces="  ";
-        final String oneSpace=" ";
+        final String twoSpaces = "  ";
+        final String oneSpace = " ";
         
-        while(str.contains(twoSpaces)) 
-        {
-            str = str.replace(twoSpaces, oneSpace);
+        while (str.contains(twoSpaces)) {  
+            str = str.replace(twoSpaces, oneSpace);      
         }
+        
         str = str.trim();
         
         return str;
